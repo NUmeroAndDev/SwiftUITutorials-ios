c3baa0c2cfea385f7998afa277f01c396b9e8ef5
NUmeroAndDev <numero.dev.and@gmail.com>
2025-02-10 16:31:23 +0900

    Finished 'Creating and combining views'

---
+++ SwiftUITutorials/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by NU mero on 2025/02/10.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
