5b353385ef32d750bb3f3af1853f6b4039420f93
NUmeroAndDev <numero.dev.and@gmail.com>
2025-02-13 20:03:30 +0900

    Finished Composing complex interfaces

--- SwiftUITutorials/Views/ContentView.swift
+++ SwiftUITutorials/Views/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab: Tab = .featured
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selectedTab) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
